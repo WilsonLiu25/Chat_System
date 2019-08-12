53a17e7ae28b1371e3257a218655b79e55c66fc8
Wilson Liu <will.25@live.com.au>
2019-08-12 15:09:04 +1200

    work on Server and Client class

--- Chat_System/src/chat_system/ClientThread.java
+++ Chat_System/src/chat_system/ClientThread.java
@@ -5,10 +5,67 @@
  */
 package chat_system;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  *
  * @author will2
  */
 public class ClientThread {
+    public static final String HOST_NAME = "localhost";
+    public static final int HOST_PORT = 7777;
+    public String userName;
+    
+    public ClientThread(){
+        
+    }
+    
+    public void startClient() {
+        Socket socket = null;
+        
+        Scanner userInput = new Scanner(System.in);
+        
+        try {
+            socket = new Socket(HOST_NAME, HOST_PORT);
+            
+            Thread readThread = new Thread(new ReadThread(this, socket));
+            readThread.start();
+            
+            Thread writeThread = new Thread(new WriteThread(this, socket));
+            writeThread.start();
+  
+                    
+        } catch (IOException e) {
+            System.err.println("Client could not successfully make a connection: " + e);
+            System.exit(-1);
+        }
+        
+//        PrintWriter printer; //output stream
+//        BufferedReader reader; //input stream
+//        
+//        try {
+//            //create a output stream for the socket
+//            printer = new PrintWriter(socket.getOutputStream(), true);
+//            
+//            //create a bufferred input stream for this socket
+//            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//           
+//            
+//        } catch (IOException e){
+//            System.out.println("Client error with games: " + e);
+//        }
+            
+        
+    }
+    
+    public static void main(String[] args) {
+        ClientThread clientThread = new ClientThread();
+        
+    }
     
 }
--- Chat_System/src/chat_system/Server.java
+++ Chat_System/src/chat_system/Server.java
@@ -5,10 +5,69 @@
  */
 package chat_system;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+
 /**
  *
  * @author will2
  */
 public class Server {
+    private int min;
+    private in max;
+    private boolean stopRequested;
+    private Random randomGenerator;
+    private static final int PORT = 7777; //hardcoded port number tbc by marker
+
+    public Server(int min, int max){
+        this.min = min;
+        this.max = max;
+        stopRequested = false;
+        randomGenerator = new Random();
+                
+    }
+    
+    public void startServer() {
+        stopRequested = false; 
+        ServerSocket serverSocket = null;
+        
+        try {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Server has started at " + InetAddress.getLocalHost() + "on port " + PORT);
+            
+        } catch (IOException e){
+            System.err.println("Server cannot listen on port: " + e);
+            System.exit(-1);
+        }
+        
+        try {
+            while (!stopRequested){
+                Socket socket = serverSocket.accept();
+                System.out.println("Connection has been made with " + socket.getInetAddress());
+                
+                ClientThread newClient = new ClientThread(this, socket);
+                Thread newClientThread = new Thread(newClient);
+                newClientThread.start();
+            }
+            serverSocket.close();
+                
+        } catch(IOException e) {
+            System.err.println("Cannot accept client connetion: " + e);
+        }
+        
+        System.out.println("Server finished");
+    }
+    
+    public void requestStop() {
+        stopRequested = true;
+    }
+    
+    public static void main(String[] args) {
+        Server server = new Server(1,100);
+        server.startServer();
+    }
     
 }
